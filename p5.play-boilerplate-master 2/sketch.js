6366aac1cf5a5df6bb82222d8e6620e477c45d44
anxnyaa <75006212+anxnyaa@users.noreply.github.com>
2020-12-09 15:18:32 +1100

    Add files via upload

---
+++ p5.play-boilerplate-master 2/sketch.js
@@ -0,0 +1,30 @@
+var movingRect,fixedRect;
+
+function setup(){
+  createCanvas(1200,800);
+
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "yellow";
+
+  fixedRect = createSprite(800,400,50,80);
+  fixedRect.shapeColor = "yellow";
+}
+function draw(){
+  background(0);
+
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 && 
+    fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 && 
+    movingRect.y - fixedRect.y < fixedRect.width/2 + movingRect.width/2 && 
+    fixedRect.y - movingRect.y < fixedRect.width/2 + movingRect.width/2 ){
+movingRect.shapeColor = "green";
+fixedRect.shapeColor = "green";
+  }
+  else{
+    movingRect.shapeColor = "yellow";
+    fixedRect.shapeColor = "yellow";
+  }
+  drawSprites();
+}
